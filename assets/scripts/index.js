7e92328e263f7ec9493275aa6252ef79225c08c4
jameskane1 <james.kane@libertymutual.com>
2017-09-27 11:26:03 -0400

    Updated organization

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-
-// const saveEvents = require('./events')
-
-const gameBoard = []
+const gamesLogic = require('./gameLogic')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -18,116 +15,15 @@ $(() => {
 // require('./example')
 
 $(() => {
-// this is the handler to display x/o
-  $('#x1y1').on('click', function () {
-    // let x1y1gameBoard = []
-    if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-      gameBoard.push('X')
-      return $(this).text('X')
-    } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-      gameBoard.push('O')
-      return $(this).text('O')
-    }
-    console.log(gameBoard)
-  }
-  )
-
-  // })
-
-  $('#x2y1').on('click', function () {
-    // let x1y1gameBoard = []
-    if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-      gameBoard.push('X')
-      return $(this).text('X')
-    } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-      gameBoard.push('O')
-      return $(this).text('O')
-    }
-    console.log(gameBoard)
-  }
-  )
-  $('#x3y1').on('click', function () {
-    // let x1y1gameBoard = []
-    if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-      gameBoard.push('X')
-      return $(this).text('X')
-    } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-      gameBoard.push('O')
-      return $(this).text('O')
-    }
-    console.log(gameBoard)
-  }
-  )
-})
-
-$('#x1y2').on('click', function () {
-// let x1y1gameBoard = []
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
-$('#x2y2').on('click', function () {
-  // let x1y1gameBoard = []
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
-// })
-
-$('#x3y2').on('click', function () {
-  // let x1y1gameBoard = []
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
-$('#x1y3').on('click', function () {
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
-// })
-
-$('#x2y3').on('click', function () {
-  // let x1y1gameBoard = []
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
-$('#x3y3').on('click', function () {
-// let x1y1gameBoard = []
-  if (gameBoard.length === 0 || gameBoard.length === 2 || gameBoard.length === 4 || gameBoard.length === 6 || gameBoard.length === 8) {
-    gameBoard.push('X')
-    return $(this).text('X')
-  } else if (gameBoard.length === 1 || gameBoard.length === 3 || gameBoard.length === 5 || gameBoard.length === 7) {
-    gameBoard.push('O')
-    return $(this).text('O')
-  }
-}
-)
+  $('#x1y1').on('click', gamesLogic.x1y1)
+  $('#x2y1').on('click', gamesLogic.x2y1)
+  $('#x3y1').on('click', gamesLogic.x3y1)
+  $('#x1y2').on('click', gamesLogic.x1y2)
+  $('#x2y2').on('click', gamesLogic.x2y2)
+  $('#x3y2').on('click', gamesLogic.x3y2)
+  $('#x1y3').on('click', gamesLogic.x1y3)
+  $('#x2y3').on('click', gamesLogic.x2y3)
+  $('#x3y3').on('click', gamesLogic.x3y3)
 // MUST US FORM ID NOT BUTTON ID!!!!
 // })
+})
